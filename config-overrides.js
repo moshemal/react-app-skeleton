4484b5249ff4f19199050589b0496fb6a889afb9
Moshe Malka <moshemal@gmail.com>
2018-02-26 17:17:48 +0200

    overide config

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,20 @@
-/* config-overrides.js */
+const paths = require('react-scripts/config/paths');
 
 module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-
+    //change configurations for both production and development
+    Object.assign(config.resolve.alias, alias);
+    
     if (env === 'production') {
-        console.log('⚡ Production build with Preact');
+        //enter production specific configurations
+    } else {
+        //enter development specific configurations
     }
 
     return config;
+};
+
+const alias = {
+    'app-src':          `${paths.appSrc}`,
+    'app-components':   `${paths.appSrc}/components`,
+    'app-scenes':       `${paths.appSrc}/scenes`
 };

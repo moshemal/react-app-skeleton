31881618160c39c90fa7d7ec566054b15dec51a5
Moshe Malka <moshemal@gmail.com>
2018-02-26 17:18:40 +0200

    add PageC

---
+++ src/scenes/PageC.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PageC extends Component {
+    render() {
+        return (
+            <div className="PageC">
+                <h1>This is PageC</h1>
+            </div>
+        );
+    }
+}
+
+export default PageC;
